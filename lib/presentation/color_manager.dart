80fc9fd2b245d058cdd471c388b8bcb299348516
Bruce Omeno <bruceo@finaplusglobal.com>
2022-09-25 19:21:45 +0300

    Initial commit

--- lib/presentation/color_manager.dart
+++ lib/presentation/color_manager.dart
@@ -5,6 +5,14 @@ class ColorManager {
   static Color darkGrey = HexColor.fromHex("#525252");
   static Color gray = HexColor.fromHex("#737477");
   static Color lightGrey = HexColor.fromHex("#9E9E9E");
+  static Color primaryOpacity70 = HexColor.fromHex("#B3ED9728");
+
+  // New colors
+  static Color darkPrimary = HexColor.fromHex("#d17d11");
+  static Color grey1 = HexColor.fromHex("#707070");
+  static Color gray2 = HexColor.fromHex("#797979");
+  static Color white = HexColor.fromHex("#FFFFFF");
+  static Color error = HexColor.fromHex("#e61f34");
 }
 
 extension HexColor on Color {
--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_design/presentation/color_manager.dart';
 
 ThemeData getApplicationTheme() {
-  return ThemeData();
+  return ThemeData(
+      // main colors
+    primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.primaryOpacity70,
+      primaryColorDark: ColorManager.darkPrimary,
+      disabledColor: ColorManager.grey1,
+      colorScheme: ColorScheme.fromSwatch().copyWith(secondary: ColorManager.gray)
+
+      // card view theme
+
+      // App bar theme
+
+      // Button theme
+
+      // Text theme
+
+      // Input decoration theme (text from field)
+      );
 }
