407d50dbb0fb5cdd0cb6ee4ee448d3e2a15c2abc
Bruce Omeno <bruceo@finaplusglobal.com>
2022-09-25 21:01:08 +0300

    Initial commit

--- lib/presentation/color_manager.dart
+++ lib/presentation/color_manager.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class ColorManager {
   static Color primary = HexColor.fromHex("#ED9728");
   static Color darkGrey = HexColor.fromHex("#525252");
-  static Color gray = HexColor.fromHex("#737477");
+  static Color grey = HexColor.fromHex("#737477");
   static Color lightGrey = HexColor.fromHex("#9E9E9E");
   static Color primaryOpacity70 = HexColor.fromHex("#B3ED9728");
 
--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -6,34 +6,57 @@ import 'package:flutter_design/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      // main colors
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.primaryOpacity70,
-      primaryColorDark: ColorManager.darkPrimary,
+    // main colors
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    colorScheme:
+        ColorScheme.fromSwatch().copyWith(secondary: ColorManager.grey),
+    // ripple color
+    splashColor: ColorManager.primaryOpacity70,
+    // card view theme
+    cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4),
+
+    // App bar theme
+    appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ColorManager.primary,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.primaryOpacity70,
+        titleTextStyle:
+            getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16)),
+
+    // Button theme
+    buttonTheme: ButtonThemeData(
+      shape: StadiumBorder(),
       disabledColor: ColorManager.grey1,
-      colorScheme:
-          ColorScheme.fromSwatch().copyWith(secondary: ColorManager.gray),
-      // ripple color
+      buttonColor: ColorManager.primary,
       splashColor: ColorManager.primaryOpacity70,
-      // card view theme
-      cardTheme: CardTheme(
-          color: ColorManager.white,
-          shadowColor: ColorManager.gray,
-          elevation: AppSize.s4),
+    ),
 
-      // App bar theme
-      appBarTheme: AppBarTheme(
-          centerTitle: true,
-          color: ColorManager.primary,
-          elevation: AppSize.s4,
-          shadowColor: ColorManager.primaryOpacity70,
-          titleTextStyle: getRegularStyle(
-              color: ColorManager.white, fontSize: FontSize.s16))
+    // elevated bottom theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            textStyle: getRegularStyle(color: ColorManager.white),
+            backgroundColor: ColorManager.primary,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(AppSize.s12)))),
 
-      // Button theme
+    // Text theme
+    textTheme: TextTheme(
+      headline1: getSemiBoldStyle(
+          color: ColorManager.darkGrey, fontSize: FontSize.s16),
+      subtitle1:
+          getMediumStyle(color: ColorManager.lightGrey, fontSize: FontSize.s14),
+      caption: getRegularStyle(color: ColorManager.grey1),
+      bodyText1: getRegularStyle(color: ColorManager.grey),
+    ),
 
-      // Text theme
+    // Input decoration theme (text from field)
 
-      // Input decoration theme (text from field)
-      );
+  );
 }
