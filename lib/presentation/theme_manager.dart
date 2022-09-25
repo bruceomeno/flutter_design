c0261ecfd35013bf05fa3f6d35c82c4bb08bd2cd
Bruce Omeno <bruceo@finaplusglobal.com>
2022-09-25 20:32:18 +0300

    Initial commit

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_design/presentation/color_manager.dart';
+import 'package:flutter_design/presentation/font_manager.dart';
+import 'package:flutter_design/presentation/styles_manager.dart';
+import 'package:flutter_design/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
       // main colors
-    primaryColor: ColorManager.primary,
+      primaryColor: ColorManager.primary,
       primaryColorLight: ColorManager.primaryOpacity70,
       primaryColorDark: ColorManager.darkPrimary,
       disabledColor: ColorManager.grey1,
-      colorScheme: ColorScheme.fromSwatch().copyWith(secondary: ColorManager.gray)
-
+      colorScheme:
+          ColorScheme.fromSwatch().copyWith(secondary: ColorManager.gray),
+      // ripple color
+      splashColor: ColorManager.primaryOpacity70,
       // card view theme
+      cardTheme: CardTheme(
+          color: ColorManager.white,
+          shadowColor: ColorManager.gray,
+          elevation: AppSize.s4),
 
       // App bar theme
+      appBarTheme: AppBarTheme(
+          centerTitle: true,
+          color: ColorManager.primary,
+          elevation: AppSize.s4,
+          shadowColor: ColorManager.primaryOpacity70,
+          titleTextStyle: getRegularStyle(
+              color: ColorManager.white, fontSize: FontSize.s16))
 
       // Button theme
 
