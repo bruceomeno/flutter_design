ba9f885473900a279a557bb8f390f5bcfd2a0eb2
Bruce Omeno <bruceo@finaplusglobal.com>
2022-09-26 17:50:41 +0300

    Initial commit

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,3 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_design/presentation/forgot_password/forgot_password.dart';
+import 'package:flutter_design/presentation/login/login.dart';
+import 'package:flutter_design/presentation/main/main.dart';
+import 'package:flutter_design/presentation/onboarding/onboarding.dart';
+import 'package:flutter_design/presentation/register/register.dart';
+import 'package:flutter_design/presentation/resources/strings_manager.dart';
+import 'package:flutter_design/presentation/splash/splash.dart';
+import 'package:flutter_design/presentation/store_details/store_details.dart';
+
 class Routes {
   static const String splashRoute = "/";
   static const String onBoardingRoute = "/onBoarding";
@@ -7,4 +17,36 @@ class Routes {
   static const String mainRoute = "/main";
   static const String storeDetailsRoute = "/storeDetails";
 }
- 
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => SplashView());
+      case Routes.onBoardingRoute:
+        return MaterialPageRoute(builder: (_) => OnBoardingView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => LoginView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => RegisterView());
+      case Routes.forgotPasswordRoute:
+        return MaterialPageRoute(builder: (_) => ForgotPasswordView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => MainView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => StoreDetailsView());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+              appBar: AppBar(
+                title: Text(AppStrings.noRouteFound),
+              ),
+              body: Center(child: Text(AppStrings.noRouteFound)),
+            ));
+  }
+}
